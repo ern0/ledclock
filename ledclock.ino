d76378b846d184796c0e2835421757a358caa086
ern0 <ern0@linkbroker.hu>
2016-01-01 20:41:22 +0100

    16 vs 32 bit issues solved, more simpler math applied, clock works

--- ledclock.ino
+++ ledclock.ino
@@ -7,7 +7,7 @@
 # endif
 
 
-static char irqdiv = 0;
+static uint8_t irqdiv = 0;
 
 # define PIN 13
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(24 + 12,PIN,NEO_GRB + NEO_KHZ800);
@@ -16,21 +16,21 @@ static char large[24] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,2
 static char small[12] = { 24,25,26,27,28,29,30,31,32,33,34,35 };
 
 # ifdef SDL_DISPLAY
-static unsigned long int clockHour = HOUR;
-static unsigned long int clockMin = MIN;
-static unsigned long int clockSec = SEC;
+static uint32_t clockHour = HOUR;
+static uint32_t clockMin = MIN;
+static uint32_t clockSec = SEC;
 # else
-static unsigned long int clockHour = 14;
-static unsigned long int clockMin = 53;
-static unsigned long int clockSec = 30;
+static uint32_t clockHour = 20;
+static uint32_t clockMin = 35;
+static uint32_t clockSec = 45;
 # endif
 
-static unsigned long int stamp;
+static uint32_t stamp;
 
-static unsigned long int pos1;
-static unsigned long int lit1;
-static unsigned long int pos2;
-static unsigned long int lit2;
+static uint32_t pos1;
+static uint32_t lit1;
+static uint32_t pos2;
+static uint32_t lit2;
 
 static bool redrawRequest = false;
 
@@ -38,184 +38,202 @@ void tick();
 void setupTimerInterrupt();
 void setStamp();
 void redrawClock();
-void calc(unsigned long int scale,unsigned long int value);
+void calc(uint32_t scale,uint32_t value);
 
 # ifdef SDL_DISPLAY
 void setupEmu();
 # endif
 
 
-	void setup() {
+  void setup() {
 
-		# ifdef SDL_DISPLAY
-			setupEmu();
-		# endif
+    # ifdef SDL_DISPLAY
+      setupEmu();
+    # endif
 
-		strip.begin();
-		strip.setBrightness(40);
-		strip.show();
+    strip.begin();
+    strip.setBrightness(40);
+    strip.show();
 
-		setStamp();
-		setupTimerInterrupt();
-		
-	} // setup()
+    setStamp();
+    setupTimerInterrupt();
+    
+  } // setup()
 
 
-	# ifdef SDL_DISPLAY
-	void setupEmu() {
+  # ifdef SDL_DISPLAY
+  void setupEmu() {
 
-		strip.emuSetGridScreenAnchor("c");
-		strip.emuSetGridScreenPercent(50);
-		strip.emuSetGridCells(36,36);
-		
-		int n = 0;
-		
-		strip.emuSetPixelPos(n++,16,0);
-		strip.emuSetPixelPos(n++,20,1);
-		strip.emuSetPixelPos(n++,24,2);
-		strip.emuSetPixelPos(n++,28,4);
-		strip.emuSetPixelPos(n++,30,8);
-		strip.emuSetPixelPos(n++,31,12);
-		strip.emuSetPixelPos(n++,32,16);
-		strip.emuSetPixelPos(n++,31,20);
-		strip.emuSetPixelPos(n++,30,24);
-		strip.emuSetPixelPos(n++,28,28);
-		strip.emuSetPixelPos(n++,24,30);
-		strip.emuSetPixelPos(n++,20,31);
-		strip.emuSetPixelPos(n++,16,32);
-		strip.emuSetPixelPos(n++,12,31);
-		strip.emuSetPixelPos(n++,8,30);
-		strip.emuSetPixelPos(n++,4,28);
-		strip.emuSetPixelPos(n++,2,24);
-		strip.emuSetPixelPos(n++,1,20);
-		strip.emuSetPixelPos(n++,0,16);
-		strip.emuSetPixelPos(n++,1,12);
-		strip.emuSetPixelPos(n++,2,8);
-		strip.emuSetPixelPos(n++,4,4);
-		strip.emuSetPixelPos(n++,8,2);
-		strip.emuSetPixelPos(n++,12,1);
-		
-		strip.emuSetPixelPos(n++,16,6);
-		strip.emuSetPixelPos(n++,21,7);
-		strip.emuSetPixelPos(n++,25,11);
-		strip.emuSetPixelPos(n++,26,16);
-		strip.emuSetPixelPos(n++,25,21);
-		strip.emuSetPixelPos(n++,21,25);
-		strip.emuSetPixelPos(n++,16,26);
-		strip.emuSetPixelPos(n++,11,25);
-		strip.emuSetPixelPos(n++,7,21);
-		strip.emuSetPixelPos(n++,6,16);
-		strip.emuSetPixelPos(n++,7,11);
-		strip.emuSetPixelPos(n++,11,7);
+    strip.emuSetGridScreenAnchor("ne");
+    strip.emuSetGridScreenPercent(20);
+    strip.emuSetGridCells(36,36);
+    
+    int n = 0;
+    
+    strip.emuSetPixelPos(n++,16,0);
+    strip.emuSetPixelPos(n++,20,1);
+    strip.emuSetPixelPos(n++,24,2);
+    strip.emuSetPixelPos(n++,28,4);
+    strip.emuSetPixelPos(n++,30,8);
+    strip.emuSetPixelPos(n++,31,12);
+    strip.emuSetPixelPos(n++,32,16);
+    strip.emuSetPixelPos(n++,31,20);
+    strip.emuSetPixelPos(n++,30,24);
+    strip.emuSetPixelPos(n++,28,28);
+    strip.emuSetPixelPos(n++,24,30);
+    strip.emuSetPixelPos(n++,20,31);
+    strip.emuSetPixelPos(n++,16,32);
+    strip.emuSetPixelPos(n++,12,31);
+    strip.emuSetPixelPos(n++,8,30);
+    strip.emuSetPixelPos(n++,4,28);
+    strip.emuSetPixelPos(n++,2,24);
+    strip.emuSetPixelPos(n++,1,20);
+    strip.emuSetPixelPos(n++,0,16);
+    strip.emuSetPixelPos(n++,1,12);
+    strip.emuSetPixelPos(n++,2,8);
+    strip.emuSetPixelPos(n++,4,4);
+    strip.emuSetPixelPos(n++,8,2);
+    strip.emuSetPixelPos(n++,12,1);
+    
+    strip.emuSetPixelPos(n++,16,6);
+    strip.emuSetPixelPos(n++,21,7);
+    strip.emuSetPixelPos(n++,25,11);
+    strip.emuSetPixelPos(n++,26,16);
+    strip.emuSetPixelPos(n++,25,21);
+    strip.emuSetPixelPos(n++,21,25);
+    strip.emuSetPixelPos(n++,16,26);
+    strip.emuSetPixelPos(n++,11,25);
+    strip.emuSetPixelPos(n++,7,21);
+    strip.emuSetPixelPos(n++,6,16);
+    strip.emuSetPixelPos(n++,7,11);
+    strip.emuSetPixelPos(n++,11,7);
 
-		for (int n = 0; n < strip.numPixels(); n++ ) {
-			strip.emuSetPixelCellSize(n,4,4);
-			strip.emuSetPixelPixGap(n,5,5);
-		}
+    for (int n = 0; n < strip.numPixels(); n++ ) {
+      strip.emuSetPixelCellSize(n,4,4);
+      strip.emuSetPixelPixGap(n,5,5);
+    }
 
-	} // setupEmu()
-	# endif
-	
-
-	void setupTimerInterrupt() {
-	
-		cli();
+  } // setupEmu()
+  # endif
   
-		//set timer2 interrupt at 1000 Hz
-		TCCR2A = 0;
-		TCCR2B = 0;
-		TCNT2 = 0;
-		
-		OCR2A = 249;
-		TCCR2A |= (1 << WGM21);
-		TCCR2B |= (1 << CS22);
-		TIMSK2 |= (1 << OCIE2A);
-			
-		sei();
 
-	} // setupTimerInterrupt()
+  void setupTimerInterrupt() {
+  
+    cli();
+  
+    //set timer2 interrupt at 1000 Hz
+    TCCR2A = 0;
+    TCCR2B = 0;
+    TCNT2 = 0;
+    
+    OCR2A = 249;
+    TCCR2A |= (1 << WGM21);
+    TCCR2B |= (1 << CS22);
+    TIMSK2 |= (1 << OCIE2A);
+      
+    sei();
 
-
-
-	ISR(TIMER2_COMPA_vect) {
-	
-		irqdiv++;
-		if (irqdiv < 10) return;
-		irqdiv = 0;
-		
-		tick();  // 100 Hz
-		
-	} // ISR()
-	
-
-	void tick() { // 100 Hz
-	
-		stamp += 1;
-		if (stamp > (24*60*60*100-1)) stamp = 0; 
-
-		redrawRequest = true;
-	
-	} // tick()
+  } // setupTimerInterrupt()
 
 
-	void loop() {
 
-		if (!redrawRequest) return;
-		redrawClock();
-		redrawRequest = false;
-	
-	} // loop
-	
+  ISR(TIMER2_COMPA_vect) {
+  
+    irqdiv++;
+    if (irqdiv < 10) return;
+    irqdiv = 0;
+    
+    tick();  // 100 Hz
+    
+  } // ISR()
+  
 
-	void setStamp() {
+  void tick() { // 100 Hz
+  
+    stamp += 1;
+    if (stamp > (24*60*60*100-1)) stamp = 0; 
 
-		if (clockHour > 11) clockHour -= 12;
-		stamp = (clockHour * 60*60*100) + (clockMin * 60*100) + (clockSec * 100);
+    redrawRequest = true;
+  
+  } // tick()
 
-	} // setStamp()
-	
-	
-	void redrawClock() {
-	
-		for (int n = 0; n < 36; n++) strip.setPixelColor(n,0,0,0);
 
-		calc(12,12*60*60*100);
+  void loop() {
 
-		for (int n = 0; n < 12; n++) {
-			if (pos1 == n) strip.setPixelColor(small[n],0,lit1 / 2,lit1);
-			if (pos2 == n) strip.setPixelColor(small[n],0,lit2 / 2,lit2);
-		}
+    if (!redrawRequest) return;
+    redrawClock();
+    redrawRequest = false;
+  
+  } // loop
+  
+  
+  void pri(uint32_t num) {
 
-		calc(24,60*60*100);
+    Serial.print((uint16_t)(num >> 0x10));
+    Serial.print(":");
+    Serial.println((uint16_t)(num & 0xFFFF));
+    
+  } // pri()
 
-		for (int n = 0; n < 24; n++) {
-			if (pos1 == n) strip.setPixelColor(large[n],lit1,0,0);
-			if (pos2 == n) strip.setPixelColor(large[n],lit2,0,0);
-		}
 
-		calc(24,60*100);
+  void setStamp() {
 
-		for (int n = 0; n < 24; n++) {
-			if (pos1 == n) strip.setPixelColor(large[n],lit1,lit1,0);
-			if (pos2 == n) strip.setPixelColor(large[n],lit2,lit2,0);
-		}
-		
-		strip.show();
-		
-	} // redrawClock() 
-	
-	
-	void calc(unsigned long int scale,unsigned long int modulo) {
+    uint32_t h;
+    uint32_t m;
+    uint32_t s;
 
-		unsigned long int value = stamp % modulo;
-		
-		pos1 = (255 * scale * value) / modulo;
-		lit2 = pos1 % 255;
-		pos1 = pos1 / 255;		
-			
-		pos2 = 1 + pos1;
-		if (pos2 == scale) pos2 = 0;
-		lit1 = 255 - lit2;		
-		
-		return;
-	} // calc()
+    h = clockHour * (60L * 60L * 100L);
+    m = clockMin * (60L * 100L);
+    s = clockSec * 100L;
+    
+    stamp = h + m + s; 
+
+  } // setStamp()
+  
+  
+  void redrawClock() {
+  
+    for (int n = 0; n < 36; n++) strip.setPixelColor(n,0,0,0);
+
+    calc(12,(uint32_t)(12L*60L*60L*100L));
+
+    for (int n = 0; n < 12; n++) {
+      if (pos1 == n) strip.setPixelColor(small[n],0,lit1 / 2,lit1);
+      if (pos2 == n) strip.setPixelColor(small[n],0,lit2 / 2,lit2);
+    }
+
+    calc(24,(uint32_t)(60L*60L*100L));
+
+    for (int n = 0; n < 24; n++) {
+      if (pos1 == n) strip.setPixelColor(large[n],lit1,0,0);
+      if (pos2 == n) strip.setPixelColor(large[n],lit2,0,0);
+    }
+
+    calc(24,(uint32_t)(60L*100L));
+
+    for (int n = 0; n < 24; n++) {
+      if (pos1 == n) strip.setPixelColor(large[n],lit1,lit1,0);
+      if (pos2 == n) strip.setPixelColor(large[n],lit2,lit2,0);
+    }
+    
+    strip.show();
+    
+  } // redrawClock() 
+  
+  
+  void calc(uint32_t scale,uint32_t modulo) {
+
+    uint32_t value = stamp % modulo;
+    
+    pos1 = (255 * scale * value) / modulo;
+    lit2 = pos1 % 255;
+    pos1 = (scale * value) / modulo;
+      
+    pos2 = 1 + pos1;
+    if (pos2 == scale) pos2 = 0;
+    lit1 = 255 - lit2;    
+    
+    return;
+  } // calc()
+
+
